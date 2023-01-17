04e33edd77b98a3b5918afc9ddddd7dc82be2919
YouHyunKim <yuhyunk7@gmail.com>
2023-01-17 14:11:44 +0900

    프로젝트 초기화

---
+++ src/main/java/shop/mtcoding/byer/ByerApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.byer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByerApplication.class, args);
+	}
+
+}
