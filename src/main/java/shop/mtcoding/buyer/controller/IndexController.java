d8c7045d294451a459521ef9e23b43a86f1d0aaa
getinthere <getinthere@naver.com>
2023-01-17 14:31:55 +0900

    프로젝트 세팅완료

---
+++ src/main/java/shop/mtcoding/buyer/controller/IndexController.java
@@ -0,0 +1,12 @@
+package shop.mtcoding.buyer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
