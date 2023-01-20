eb86d90feaed8f484bca20ae6f771100a8182b0c
YouHyunKim <yuhyunk7@gmail.com>
2023-01-20 12:55:46 +0900

    구매 목록 수정 완료

--- src/main/java/shop/mtcoding/buyer/service/PurchaseService.java
+++ src/main/java/shop/mtcoding/buyer/service/PurchaseService.java
@@ -22,28 +22,28 @@ public class PurchaseService {
 
     @Transactional
     // 독립성을 가진다
+    // 비지니스로직은 순서도도 중요하다
+    // 구매가 되고 히스토리가 남겨져야한다
     public int 구매하기(int principalId, int productId, int count) {
-        // 2. 구매 히스토리 남기기(insert)
-        int result = purchaseRepository.insert(principalId, productId, count);
-        if (result != 1) {
-            return -1;
-        }
-
-        // 3. 상품 존재 확인 -> 존재가 있는 걸로 변경이 가능하게 만든다
-        // prodcutId를 아무거나 날릴 수 있다(신뢰할 수 없다)->검증이 필요하다
+        // 1. 상품 존재 확인
         Product product = productRepository.findById(productId);
         if (product == null) {
             return -1;
         }
-        // 4. 수량 체크
+        // 2. 수량 체크
         if (product.getQty() < count) {
-            return -1;
+            throw new RuntimeException();
         }
-        // 5. 재고 수량 변경
+        // 3. 재고 수량 변경
         int result2 = productRepository.updateById(product.getId(), product.getName(), product.getPrice(),
                 product.getQty() - count);
 
         if (result2 != 1) {
+            return -1;
+        }
+        // 4. 구매 히스토리 남기기(insert)
+        int result = purchaseRepository.insert(principalId, productId, count);
+        if (result != 1) {
             return -1;
         }
         return 1;
--- src/main/webapp/WEB-INF/view/product/detail.jsp
+++ src/main/webapp/WEB-INF/view/product/detail.jsp
@@ -20,15 +20,17 @@
                 <td>${product.createdAtToString}</td>
             </tr>
         </table>
-        <form action="/purchase/insert" method="post">
-            <!-- <input type="hidden" name="userId" value="${principal.id}"> -->
-            <input type="hidden" name="productId" value="${product.id}">
-            <select name="count">
-                <c:forEach begin="1" end="${product.qty}" var="num">
-                    <option value="${num}">${num}</option>
-                </c:forEach>
-            </select>
-            <button type="submit">구매하기</button>
-        </form>
+        <c:if test="${principal != null}}" >
+            <form action="/purchase/insert" method="post">
+                <!-- <input type="hidden" name="userId" value="${principal.id}"> -->
+                <input type="hidden" name="productId" value="${product.id}">
+                <select name="count">
+                    <c:forEach begin="1" end="${product.qty}" var="num">
+                        <option value="${num}">${num}</option>
+                    </c:forEach>
+                </select>
+                <button type="submit">구매하기</button>
+            </form>
+        </c:if>
 
         <%@ include file="../layout/footer.jsp" %>
